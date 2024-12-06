72e753dc83122cab80eb6f53b1eb362417e25348
Brett Kuskie <fullaxx@gmail.com>
2024-12-06 12:09:14 +0000

    Add config json for tables and symbols

--- create_html.py
+++ create_html.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import time
+from pathlib import Path
 
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
@@ -17,33 +18,34 @@ def write_to_file(text, filename):
 		f.write(text)
 		f.close()
 
-def gen_html_table(tbl):
-	table_name,symbols = tbl.split('=')
-	symb_list = symbols.split(',')
+def gen_html_table(k, v):
+	table_name = v['table_name']
+	symbols_str = v['symbols']
+	symbols_list = symbols_str.split(',')
 
 	html = '<table>'
 
 	html += '<tr>'
 	html += f'<th><u>{table_name}</u></th>'
-	for symb in symb_list:
+	for symb in symbols_list:
 		html += f'<th id={symb}_th><a href=https://finance.yahoo.com/quote/{symb}>{symb}</a></th>'
 	html += '</tr>'
 
 	html += '<tr>'
 	html += '<td>$</td>'
-	for symb in symb_list:
+	for symb in symbols_list:
 		html += f'<td id={symb}_currentPrice></td>'
 	html += '</tr>'
 
 	html += '<tr>'
 	html += '<td>%</td>'
-	for symb in symb_list:
+	for symb in symbols_list:
 		html += f'<td id={symb}_move></td>'
 	html += '</tr>'
 
 	html += '<tr>'
 	html += '<td>Close</td>'
-	for symb in symb_list:
+	for symb in symbols_list:
 		html += f'<td id={symb}_previousClose></td>'
 	html += '</tr>'
 
@@ -51,7 +53,7 @@ def gen_html_table(tbl):
 	if (mc_toggle is not None) and (mc_toggle == '1'):
 		html += '<tr>'
 		html += '<td>MCap</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td id={symb}_mcap></td>'
 		html += '</tr>'
 
@@ -59,7 +61,7 @@ def gen_html_table(tbl):
 	if (fpe_toggle is not None) and (fpe_toggle == '1'):
 		html += '<tr>'
 		html += '<td>FPE</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td id={symb}_forwardPE></td>'
 		html += '</tr>'
 
@@ -67,7 +69,7 @@ def gen_html_table(tbl):
 	if (pst12_toggle is not None) and (pst12_toggle == '1'):
 		html += '<tr>'
 		html += '<td>PST12</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td id={symb}_priceToSalesTrailing12Months></td>'
 		html += '</tr>'
 
@@ -75,7 +77,7 @@ def gen_html_table(tbl):
 	if (peg_toggle is not None) and (peg_toggle == '1'):
 		html += '<tr>'
 		html += '<td>tPEG</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td id={symb}_trailingPegRatio></td>'
 		html += '</tr>'
 
@@ -83,7 +85,7 @@ def gen_html_table(tbl):
 	if (pb_toggle is not None) and (pb_toggle == '1'):
 		html += '<tr>'
 		html += '<td>PB</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td id={symb}_pbRatio></td>'
 		html += '</tr>'
 
@@ -91,7 +93,7 @@ def gen_html_table(tbl):
 	if (urls_toggle is not None) and (urls_toggle == '1'):
 		html += '<tr>'
 		html += '<td>URLS</td>'
-		for symb in symb_list:
+		for symb in symbols_list:
 			html += f'<td><a href=https://finance.yahoo.com/quote/{symb}>YF</a>/<a href=https://gurufocus.com/stock/{symb}>GF</a></td>'
 		html += '</tr>'
 
@@ -114,42 +116,48 @@ def gen_html_head(market_data_fetch_interval):
 	html += '</head>'
 	return html
 
-def gen_html_body():
+def gen_html_body(cfg):
 	html = '<body>'
 	html += '<center>'
 	html += '<h2>Stonk Stalker</h2>'
 	html += '<h3><div id="time"></div></h3>'
-	for tbl in tables_list:
-		html += gen_html_table(tbl)
+	for k,v in cfg.items():
+		html += gen_html_table(k,v)
 	html += '<a href="https://github.com/Fullaxx/stonk_stalker_redis">Source Code on GitHub</a>'
 	html += '</center>'
 	html += '</body>'
 	return html
 
-def gen_index_html(tables_list, market_data_fetch_interval):
+def gen_index_html(cfg, market_data_fetch_interval):
 	html = '<!DOCTYPE html>'
 	html += '<html lang="en">'
 	html += gen_html_head(market_data_fetch_interval)
-	html += gen_html_body()
+	html += gen_html_body(cfg)
 	html += '</html>'
 	write_to_file(html, 'index.html')
+
+def read_tables_config():
+	cfg_dir = Path('/config')
+	if not cfg_dir.is_dir():
+		bailmsg(f'/config is not a directory!')
+	cfg_file = Path('/config/tables.json')
+	if not cfg_file.is_file():
+		bailmsg(f'/config/tables.json is not a file!')
+
+	with open('/config/tables.json', 'r') as f:
+		config_str = f.read()
+		config = json.loads(config_str)
+
+	return config
 
 if __name__ == '__main__':
 	wwwdir = os.getenv('WWWDIR')
 	if wwwdir is not None: os.chdir(wwwdir)
 
-	ticker_tables = os.getenv('TICKER_TABLES')
-	if ticker_tables is None: bailmsg('Set TICKER_TABLES')
-
 	market_data_fetch_interval = os.getenv('MARKET_DATA_FETCH_INTERVAL')
 	if market_data_fetch_interval is None: market_data_fetch_interval = '4000'
 
-	symb_list = []
-	tables_list = ticker_tables.split(';')
-	for tbl in tables_list:
-		symbols = tbl.split('=')[1]
-		symb_list += symbols.split(',')
-
-	gen_index_html(tables_list, market_data_fetch_interval)
+	tables_config = read_tables_config()
+	gen_index_html(tables_config, market_data_fetch_interval)
 	time.sleep(2)
 #	Sleep for a bit so supervisord knows all is well
--- yf2redis_launcher.py
+++ yf2redis_launcher.py
@@ -2,7 +2,9 @@
 
 import os
 import sys
+import json
 import time
+from pathlib import Path
 
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
@@ -11,21 +13,32 @@ def bailmsg(*args, **kwargs):
 	eprint(*args, **kwargs)
 	sys.exit(1)
 
-if __name__ == '__main__':
-	ticker_tables = os.getenv('TICKER_TABLES')
-	if ticker_tables is None: bailmsg('Set TICKER_TABLES')
+def read_tables_config():
+	cfg_dir = Path('/config')
+	if not cfg_dir.is_dir():
+		bailmsg(f'/config is not a directory!')
+	cfg_file = Path('/config/tables.json')
+	if not cfg_file.is_file():
+		bailmsg(f'/config/tables.json is not a file!')
 
+	with open('/config/tables.json', 'r') as f:
+		config_str = f.read()
+		config = json.loads(config_str)
+
+	return config
+
+if __name__ == '__main__':
 	ri_str = os.getenv('YF_REQUEST_INTERVAL')
 	request_interval = 30 if ri_str is None else int(ri_str)
 
-	symb_list = []
-	tables_list = ticker_tables.split(';')
-	for tbl in tables_list:
-		symbols = tbl.split('=')[1]
-		symb_list += symbols.split(',')
+	symbols_list = []
+	tables_config = read_tables_config()
+	for k,v in tables_config.items():
+		symbols_str = v['symbols']
+		symbols_list += symbols_str.split(',')
 
 #	When this loop is done, it will exit 0
 #	Current Design: supervisord will restart it automatically
-	for symb in symb_list:
-		os.system(f'/app/yf2redis.py -s {symb}')
+	for symbol in symbols_list:
+		os.system(f'/app/yf2redis.py -s {symbol}')
 		time.sleep(request_interval)
