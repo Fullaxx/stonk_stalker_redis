892e3125ef056fbe6910dfbeb4a3bfa01576e925
Brett Kuskie <fullaxx@gmail.com>
2024-12-13 14:45:18 +0000

    remove unused dtr class from create_html.py

--- 02_webserver/create_html.py
+++ 02_webserver/create_html.py
@@ -104,7 +104,7 @@ def gen_html_table(k, table_name, table_type, symbols_str, dc):
 			html += '<td>DTR</td>'
 			for symb in symbols_list:
 				fsymb = symb.replace('/','-')
-				html += f'<td id={fsymb}_dtr class=dtr></td>'
+				html += f'<td id={fsymb}_dtr></td>'
 			html += '</tr>'
 
 	if dc['DISPLAY_OTHER_URLS']:
