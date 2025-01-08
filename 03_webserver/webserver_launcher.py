8507c58066233beac5131d95269f4b3f9a02d838
Brett Kuskie <fullaxx@gmail.com>
2025-01-08 13:27:02 +0000

    Simplify time calculations in 03_webserver/webserver_launcher.py

--- 03_webserver/webserver_launcher.py
+++ 03_webserver/webserver_launcher.py
@@ -34,60 +34,60 @@ def bailmsg(*args, **kwargs):
 	eprint(*args, **kwargs)
 	sys.exit(1)
 
-def launch_script(path, now_z, verbose):
+def launch_script(path, verbose):
 	if (g_debug_python or verbose):
-		now_et = now_z.astimezone(g_tz_et)
+		now_et = g_now_z.astimezone(g_tz_et)
 		timestamp_str = now_et.strftime('%Y-%m-%d %H:%M:%S')
 		print(f'{timestamp_str}: {path}', flush=True)
 	os.system(path)
 
 #	Get the most recent value for ALPACA:MARKET:NEXTOPEN:ZSTAMP from redis
 #	If it has changed, print a message and update g_nextopen_zs
-def update_next_open(r, now_z, now_s):
+def update_next_open():
 	global g_nextopen_zs
 
-	nextopen_zstamp_str = r.get('ALPACA:MARKET:NEXTOPEN:ZSTAMP')
+	nextopen_zstamp_str = g_rc.get('ALPACA:MARKET:NEXTOPEN:ZSTAMP')
 	if nextopen_zstamp_str is None: return
 	nextopen_zs = int(nextopen_zstamp_str)
 	if (nextopen_zs != g_nextopen_zs):
 		g_nextopen_zs = nextopen_zs
-		nosa = g_nextopen_zs - now_s
-		now_et = now_z.astimezone(g_tz_et)
+		nosa = g_nextopen_zs - g_now_s
+		now_et = g_now_z.astimezone(g_tz_et)
 		log_timestamp_str = now_et.strftime('%Y-%m-%d %H:%M:%S')
 		print(f'{log_timestamp_str}: next_open={g_nextopen_zs} {nosa} seconds away', flush=True)
 
 #	Calculate how far away we are from the market open using g_next_open_timestamp
 #	If we are within 1 second, launch ./update_prevclose.py
-def prepare_for_upcoming_market_open(now_z, now_s):
+def prepare_for_upcoming_market_open():
 	global g_nextopen_zs
 	if (g_nextopen_zs == 0): return
-	nosa = g_nextopen_zs - now_s
+	nosa = g_nextopen_zs - g_now_s
 	if g_debug_python:
 		print(f'{nosa} seconds away')
 	if (nosa == 1):
-		launch_script('./update_prevclose.py', now_z, True)
+		launch_script('./update_prevclose.py', True)
 
-def every_hour(r, now_z):
-	launch_script('./create_html.py', now_z, True)
+def every_hour():
+	launch_script('./create_html.py', True)
 
-def every_halfhour(now_z, now_s):
-	update_next_open(r, now_z, now_s)
+def every_halfhour():
+	update_next_open()
 
-#def every_minute(now_z):
+#def every_minute(g_now_z):
 #	pass
 
-def every_second(now_z, now_s):
+def every_second():
 	global g_last_mdjc_trigger
 
-	prepare_for_upcoming_market_open(now_z, now_s)
-	launch_script('./create_market_status_json.py', now_z, False)
-	if (now_s >= (g_last_mdjc_trigger + g_market_json_creation_interval)):
-		launch_script('./create_market_data_json.py', now_z, False)
-		g_last_mdjc_trigger = now_s
+	prepare_for_upcoming_market_open()
+	launch_script('./create_market_status_json.py', False)
+	if (g_now_s >= (g_last_mdjc_trigger + g_market_json_creation_interval)):
+		launch_script('./create_market_data_json.py', False)
+		g_last_mdjc_trigger = g_now_s
 
 #	Load MARKET_DATA_CREATE_INTERVAL from config
-def load_market_json_creation_interval(r):
-	cfg_str = r.get('DASHBOARD:CONFIG')
+def load_market_json_creation_interval():
+	cfg_str = g_rc.get('DASHBOARD:CONFIG')
 	ss_config = json.loads(cfg_str)
 	dash_config = ss_config['DASHBOARD_CONFIG']
 	interval = dash_config['MARKET_DATA_CREATE_INTERVAL']
@@ -128,21 +128,21 @@ def chdir_if_production():
 if __name__ == '__main__':
 	chdir_if_production()
 	redis_url = acquire_environment()
-	r = connect_to_redis(redis_url, True, False, g_debug_python)
+	g_rc = connect_to_redis(redis_url, True, False, g_debug_python)
 
 	if g_wait_for_ready:
-		wait_for_ready(r, 'DASHBOARD:READY', 0.1)
+		wait_for_ready(g_rc, 'DASHBOARD:READY', 0.1)
 
-	g_market_json_creation_interval = load_market_json_creation_interval(r)
+	g_market_json_creation_interval = load_market_json_creation_interval()
 
 #	Tasks to do once at startup
-	now_z = datetime.datetime.now(g_tz_utc)
-	now_s = int(now_z.timestamp())
-	update_next_open(r, now_z, now_s)
-	launch_script('./create_html.py', now_z, False)
-	launch_script('./create_market_status_json.py', now_z, False)
-	launch_script('./create_market_data_json.py', now_z, False)
-	g_last_mdjc_trigger = int(now_z.timestamp())
+	g_now_z = datetime.datetime.now(g_tz_utc)
+	g_now_s = int(g_now_z.timestamp())
+	update_next_open()
+	launch_script('./create_html.py', False)
+	launch_script('./create_market_status_json.py', False)
+	launch_script('./create_market_data_json.py', False)
+	g_last_mdjc_trigger = g_now_s
 
 	signal.signal(signal.SIGINT,  signal_handler)
 	signal.signal(signal.SIGTERM, signal_handler)
@@ -153,22 +153,22 @@ if __name__ == '__main__':
 	g_last_halfhour_trigger = 0
 	g_last_hour_trigger = 0
 	while not g_shutdown:
-		now_z = datetime.datetime.now(g_tz_utc)
-		now_s = int(now_z.timestamp())
-		if (now_s > g_last_sec_trigger):
-			every_second(now_z, now_s)
-			g_last_sec_trigger = now_s
-#		if ((now_s % 60) == 0):
-#			if (now_s > g_last_min_trigger):
-#				every_minute(now_z)
-#				g_last_min_trigger = now_s
-		if ((now_s % 1800) == 0):
-			if (now_s > g_last_halfhour_trigger):
-				every_halfhour(now_z)
-				g_last_halfhour_trigger = now_s
-		if ((now_s % 3600) == 0):
-			if (now_s > g_last_hour_trigger):
-				every_hour(r, now_z)
-				g_last_hour_trigger = now_s
+		g_now_z = datetime.datetime.now(g_tz_utc)
+		g_now_s = int(g_now_z.timestamp())
+		if (g_now_s > g_last_sec_trigger):
+			every_second()
+			g_last_sec_trigger = g_now_s
+#		if ((g_now_s % 60) == 0):
+#			if (g_now_s > g_last_min_trigger):
+#				every_minute(g_now_z)
+#				g_last_min_trigger = g_now_s
+		if ((g_now_s % 1800) == 0):
+			if (g_now_s > g_last_halfhour_trigger):
+				every_halfhour()
+				g_last_halfhour_trigger = g_now_s
+		if ((g_now_s % 3600) == 0):
+			if (g_now_s > g_last_hour_trigger):
+				every_hour()
+				g_last_hour_trigger = g_now_s
 
 		usleep(1000)
